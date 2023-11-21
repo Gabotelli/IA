ffd418580d34ae34ec0ed1f5565b9cf6affa0814
Gabotelli <suarez.gabriel03@gmail.com>
2023-11-21 13:00:32 +0100

    klk

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Buenos dias")
+#import modules for graphs
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+# import modules for data
+import requests
+import json
+import csv
+import os
+import time
+# import modules for data cleaning
+import datetime as dt
+from datetime import datetime
+import calendar
+import re
+# import modules for data analysis
+from scipy import stats
+import scipy.stats as sts
+import scipy.stats as sem
+
+
