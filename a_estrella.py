eee964abee4b597c5709999c5510f77a3dc367f8
Zeus-303 <alejandroperisacedo@gmail.com>
2023-12-08 20:00:02 +0100

    Casi terminada la cosa

--- a_estrella.py
+++ a_estrella.py
@@ -1,14 +1,14 @@
 from heapq import heappop, heappush
 
-def a_star(grafo, nodoInicio, nodoObjetivo, heuristica, modoObjetivo = None):
-    listaBusqueda = [(0, None, nodoInicio, 0)] #CosteF, Linea, Nodo, transbordos
+def a_star(grafo, nodoInicio, nodoObjetivo, heuristica, horaSalida):
+    listaBusqueda = [(0, None, nodoInicio, horaSalida)] #CosteF, Linea, Nodo, transbordos
     costeG = {nodoInicio: 0}
     costeF = {nodoInicio: 0}
     caminoAnterior = {nodoInicio: None}
 
     while listaBusqueda:
         #Elige el nodo con el coste menor
-        _, lineaActual, nodoActual, transbordos = heappop(listaBusqueda)
+        _, lineaActual, nodoActual, transbordos, hora = heappop(listaBusqueda)
 
         #Si el nodo es el nodo Objetivo, hemos terminado y
         #devolvemos el camino que nos ha llevado hasta el
@@ -24,17 +24,19 @@ def a_star(grafo, nodoInicio, nodoObjetivo, heuristica, modoObjetivo = None):
         for nodoVecino in grafo.neighbors(nodoActual):
             #Calculamos el coste de ir desde el nodo actual hasta el vecino
             costeGVecino = costeG[nodoActual] + grafo.edges[nodoActual, nodoVecino]['weight']
-            h, transbordosAux= heuristica(nodoVecino, nodoObjetivo, nodoActual, lineaActual, transbordos)
+            h, transbordosAux, variacionTiempo= heuristica(nodoVecino, nodoObjetivo, nodoActual, lineaActual, transbordos)
             costeFVecino = costeGVecino + h
             #Miramos si el camino actual es mejor que el anterior o si el vecino no ha sido visitado
             if nodoVecino not in costeG or costeFVecino < costeF[nodoVecino]:
                 #Actualizamos el coste del vecino
                 costeG[nodoVecino] = costeGVecino
                 costeF[nodoVecino] = costeFVecino
+                #Actualizamos la hora de llegada al nodo vecino
+                horaAux = hora + variacionTiempo
                 #Linea en comun entre nodoActual y nodoVecino
                 lineaAux = str((set(grafo.nodes[nodoVecino]['linea']) & set(grafo.nodes[nodoActual]['linea']) ).pop())
                 #Añadimos el vecino a la lista de busqueda
-                heappush(listaBusqueda, (costeFVecino, lineaAux,nodoVecino,transbordosAux))
+                heappush(listaBusqueda, (costeFVecino, lineaAux,nodoVecino,transbordosAux,horaAux))
                 #Guardamos el camino que nos ha llevado hasta el vecino
                 caminoAnterior[nodoVecino] = nodoActual
 
